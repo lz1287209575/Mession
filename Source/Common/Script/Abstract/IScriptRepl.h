9a15fc49109c5f7a11af9adffe7262c312731cff
padgetthan <padgetthan@tencent.com>
2026-08-05 09:31:19 +0000

    feat(script): IScriptRepl unified REPL interface

---
+++ Source/Common/Script/Abstract/IScriptRepl.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Common/Runtime/MLib.h"
+#include "Common/Runtime/Object/Result.h"
+#include "Common/Script/Abstract/EScriptLanguage.h"
+#include "Common/Script/Abstract/IScriptEngine.h"
+
+namespace mession::script {
+
+// IScriptRepl 统一 REPL 接口
+// 前端:WebSocket(走 HttpDebugServer)或 TCP(stdin/stdout 转发)
+// 多 VM 并存:每个 REPL session 绑定一个 IScriptEngine 实例
+class IScriptRepl
+{
+public:
+    virtual ~IScriptRepl() = default;
+
+    virtual bool               Connect(TSharedPtr<IScriptEngine> Engine) = 0;
+    virtual bool               ReadLine(MString& OutLine)                = 0;
+    virtual void               WriteLine(TStringView Line)               = 0;
+    virtual TResult<MString>   Eval(TStringView Code)                    = 0;
+    virtual void               Interrupt()                               = 0;
+    virtual void               Disconnect()                              = 0;
+
+    virtual EScriptLanguage GetLanguage() const = 0;
+};
+
+} // namespace mession::script
