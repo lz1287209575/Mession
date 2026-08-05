b712cfc6c97f11bcce57cb6f5941b8a4b6e571fe
padgetthan <padgetthan@tencent.com>
2026-08-05 16:01:48 +0000

    feat(lua): LuaScriptState wrapper around lua_State + load buffer

---
+++ Source/Common/Script/Lua/LuaScriptState.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Common/Runtime/MLib.h"
+
+extern "C" {
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+}
+
+namespace mession::script::lua {
+
+class MLuaScriptState
+{
+public:
+    MLuaScriptState();
+    ~MLuaScriptState();
+
+    MLuaScriptState(const MLuaScriptState&) = delete;
+    MLuaScriptState& operator=(const MLuaScriptState&) = delete;
+
+    lua_State* GetLuaState() const { return L; }
+    bool IsValid() const { return L != nullptr; }
+
+    // 加载并执行字节流;返回 Err("") 表示成功
+    MString LoadBuffer(const MString& Name, const char* Bytes, size_t Size);
+
+private:
+    lua_State* L = nullptr;
+};
+
+} // namespace mession::script::lua
