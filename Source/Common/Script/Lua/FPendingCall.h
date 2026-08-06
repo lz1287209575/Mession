f5c1d832d58ada34ef20972462216aeb37d250d9
padgetthan <padgetthan@tencent.com>
2026-08-06 04:23:11 +0000

    feat(lua): LuaCoroutineBridge yield/resume + tests passing

---
+++ Source/Common/Script/Lua/FPendingCall.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "Common/Runtime/MLib.h"
+
+extern "C" {
+struct lua_State;
+}
+
+namespace mession::script::lua {
+
+class FLuaPendingCall
+{
+public:
+    FLuaPendingCall() = default;
+    ~FLuaPendingCall() = default;
+
+    FLuaPendingCall(const FLuaPendingCall&)            = delete;
+    FLuaPendingCall& operator=(const FLuaPendingCall&) = delete;
+
+    void InitYield(lua_State* InL, int32 InNArgs);
+    void Resume(const MString* OutVals, size_t OutCount, MString* OutErrs, size_t ErrCount);
+
+    bool IsPending() const { return bPending; }
+    void Cancel() { bPending = false; }
+
+private:
+    lua_State* L = nullptr;
+    int32      NArgs   = 0;
+    int32      ThreadRef = -1;
+    bool       bPending = false;
+};
+
+} // namespace mession::script::lua
---
+++ Source/Common/Script/Lua/LuaCoroutineBridge.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Common/Runtime/MLib.h"
+#include "Common/Runtime/Object/Result.h"
+#include "Common/Script/Lua/FPendingCall.h"
+
+extern "C" {
+#include <lua.h>
+}
+
+namespace mession::script::lua {
+
+class MLuaCoroutineBridge
+{
+public:
+    static bool IsCoroutineYielded(lua_State* L);
+    static void ResumeFromStack(lua_State* L, int32 NResults);
+    static void ThrowError(lua_State* L, const MString& Msg);
+};
+
+} // namespace mession::script::lua
